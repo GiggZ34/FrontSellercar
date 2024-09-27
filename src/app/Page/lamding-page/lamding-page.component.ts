0be7d9b37898e9e7c3e89a2fd61c678d6804a61c
Matthias <matthias.begot@gmail.com>
2024-09-27 13:58:21 +0200

    first request. Error Cors

--- src/app/Page/lamding-page/lamding-page.component.ts
+++ src/app/Page/lamding-page/lamding-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiManagementService} from "../../Services/api-management.service";
 
 @Component({
   selector: 'app-lamding-page',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './lamding-page.component.html',
   styleUrl: './lamding-page.component.scss'
 })
-export class LamdingPageComponent {
+export class LamdingPageComponent implements OnInit {
 
+  constructor(private request: ApiManagementService) {
+  }
+
+  ngOnInit() {
+    this.request.get("api/car_models/").then(data => {
+      console.log(data);
+    },
+    error=>{
+      console.log(error)
+    }
+    )
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {LamdingPageComponent} from "./Page/lamding-page/lamding-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    component:LamdingPageComponent
+  }
+];
