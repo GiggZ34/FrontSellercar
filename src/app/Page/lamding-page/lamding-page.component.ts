ee3aeca23f951686bb157f4f283cd59b4dad2750
guillaume.rivacan@gcatrans.com <guillaume.rivalan@gmail.com>
2024-09-27 10:38:15 +0200

    LamdingPage

---
+++ src/app/Page/lamding-page/lamding-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lamding-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './lamding-page.component.html',
+  styleUrl: './lamding-page.component.scss'
+})
+export class LamdingPageComponent {
+
+}
