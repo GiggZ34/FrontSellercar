b47ef235c40f6ec45cf623ab088954bcd2fdb561
emile <emilepicard323@gmail.com>
2024-10-16 11:34:21 +0200

    ajout du trie

--- src/app/Page/display-seller/display-seller.component.ts
+++ src/app/Page/display-seller/display-seller.component.ts
@@ -18,6 +18,8 @@ import {Router} from "@angular/router";
 import {MatButton} from "@angular/material/button";
 import {CanvasJSAngularChartsModule} from "@canvasjs/angular-charts";
 import {CheckConnexionService} from "../../Services/check-connexion.service";
+import {MatSort, MatSortHeader} from "@angular/material/sort";
+import {NgIf} from "@angular/common";
 
 
 @Component({
@@ -35,7 +37,10 @@ import {CheckConnexionService} from "../../Services/check-connexion.service";
     MatHeaderRowDef,
     MatRowDef,
     MatButton,
-    CanvasJSAngularChartsModule
+    CanvasJSAngularChartsModule,
+    MatSort,
+    MatSortHeader,
+    NgIf
   ],
   templateUrl: './display-seller.component.html',
   styleUrl: './display-seller.component.scss'
@@ -46,7 +51,17 @@ export class DisplaySellerComponent implements  OnInit {
 
   displayedColumns: string[] = ['id', 'roles', 'first_name', 'last_name', 'username', 'concession'];
 
-  @ViewChild(MatTable) table!: MatTable<any>;
+  counters: { [key: string]: number } = {
+    id: 0,
+    roles: 0,
+    first_name: 0,
+    last_name: 0,
+    username: 0,
+    concession: 0,
+  };
+
+
+  @ViewChild(MatSort) sort!: MatSort;
 
   chartOptions: ChartOptionsInterface = {
     title: {
@@ -63,27 +78,22 @@ constructor(private displaySellerService: ServiceDisplaySellerService,
             private connexion : CheckConnexionService ) { }
 
   ngOnInit() {
-    this.displayUser()
+    this.SortUser()
     this.getAllUserStat()
     this.connexion.checkConnexion()
   }
 
-  displayUser(){
-    this.displaySellerService.FuncDisplay().then((data:InterDisplaySeller[] | undefined)=>{
+ async SortUser() {
+      const data = await this.displaySellerService.FuncDisplay(null);
+
       if(data) {
         this.dataSource.data = data;
       }
+    }
 
-      this.table.renderRows();
-
-    })
-      .catch((error)=>{
-        console.log('Erreur',error)
-      });
-  }
 
   redirection(row:InterDisplaySeller ){
-    this.router.navigate([`all-sale/seller/${row.id}`]);
+    //this.router.navigate([`all-sale/seller/${row.id}`]);
   }
 
   Return(){
@@ -112,6 +122,26 @@ constructor(private displaySellerService: ServiceDisplaySellerService,
         })) : []
       }]
     };
+  }
+
+  async Order(input: string) {
+    const validInputs = ["id", "roles", "first_name", "last_name", "username", "concession"];
+
+    if (validInputs.includes(input)) {
+      const actions = [
+        async () => await this.displaySellerService.FuncDisplay(input),
+        async () => await this.displaySellerService.FuncDisplay("-" + input),
+        async () => await this.displaySellerService.FuncDisplay(null),
+      ];
+
+      const data = await actions[this.counters[input]]();
+
+      this.counters[input] = (this.counters[input] + 1) % actions.length;
+
+      if (data) {
+        this.dataSource.data = data;
+      }
+    }
   }
 
 
